1111cf67602a22f56ecfaeba37c8e2cdc9d20a0b
Mark Ferrell <major@homeonderanged.org>
2026-01-27 21:47:35 -0800

    fix: update copyright headers to Mark Ferrell
    Updated copyright holder from 'Crusty Programming Language' to 'Mark Ferrell' in:
    - All source files (src/*.rs)
    - LICENSE.txt
    
    This ensures proper attribution of the project's copyright.

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Crusty Programming Language
+// Copyright (c) 2026 Mark Ferrell
 // Licensed under the MIT License. See LICENSE.txt in the project root.
 
 //! Command-line interface module for crustyc compiler.
